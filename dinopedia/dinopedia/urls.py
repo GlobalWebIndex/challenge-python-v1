74fe93e02eaa6d562a3f5b61f5f27037e2d6ceb7
Pavla Vesela <paja.v7@gmail.com>
2022-05-30 13:48:16 +0200

    black and postgres dependencies

--- dinopedia/dinopedia/urls.py
+++ dinopedia/dinopedia/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('dinopolls/', include('dinopolls.urls')),
-    path('admin/', admin.site.urls),
+    path("dinosaurs/", include("dinosaurs.urls")),
+    path("admin/", admin.site.urls),
 ]
