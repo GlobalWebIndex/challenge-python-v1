e1e60389c98b198360b4113262cbd7f931dc2dd1
nichaos2 <nick.ioannou86@gmail.com>
2022-07-23 14:57:48 +0300

    first model to test the persistence in docker

--- dinopedia/dinosaurs/admin.py
+++ dinopedia/dinosaurs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from dinosaurs.models import DinoOwner
+
+admin.site.register(DinoOwner)
--- dinopedia/dinosaurs/models.py
+++ dinopedia/dinosaurs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class DinoOwner(models.Model):
+    name = models.TextField(null=False)
+
+    def __str__(self):
+        return self.name
