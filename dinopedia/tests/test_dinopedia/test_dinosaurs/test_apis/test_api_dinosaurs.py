ba684ed76b0d73c55b8683cf48402165c26cf876
nichaos2 <nick.ioannou86@gmail.com>
2022-07-24 19:52:31 +0300

    add delete test

--- dinopedia/tests/test_dinopedia/test_dinosaurs/test_apis/test_api_dinosaurs.py
+++ dinopedia/tests/test_dinopedia/test_dinosaurs/test_apis/test_api_dinosaurs.py
@@ -42,5 +42,24 @@ class TestDinosaurEndpoint:
         assert 'duplicate key value violates unique constraint "unique_dinosaur_name"' in excinfo.value.args[0]
     
     # TODO create tests for every orther conflict
-    
+
     # TODO test to put, patch, delete
+
+    @pytest.mark.django_db
+    def test_delete(self, api_client, admin):
+
+        # make 2
+        quantity = 2
+        results = baker.make_recipe(self.dino_recipe, _quantity = quantity)
+
+        # delete 1
+        response = api_client.delete(f"{self.endpoint}/{results[0].id}")
+
+        assert response.status_code == 204
+        
+        # check that we have one left
+        response = api_client.get(self.endpoint)
+        results = json.loads(response.content)["results"]
+
+        assert response.status_code == 200
+        assert len(results) == quantity-1
